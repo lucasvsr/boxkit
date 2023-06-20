9622a250e7b123bd5c8b98cc0efa208d3cef9dfc
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2023-06-20 08:23:25 -0300

    Update chaotic-aur.sh

--- scripts/chaotic-aur.sh
+++ scripts/chaotic-aur.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --init
-sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
