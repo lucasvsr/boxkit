21aac9a8350d53343b07f55ef44f6e04602bb9cc
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2024-01-30 09:12:22 -0300

    Update imports.sh

--- scripts/imports.sh
+++ scripts/imports.sh
@@ -4,7 +4,7 @@ set -oue pipefail
 
 CONFIGURATIONS_FILE=/tmp/conf.yml
 
-readarray PACKAGES_IMPORT < <(yq -o=j -I=0 '.imports.packages[]' "$CONFIGURATIONS_FILE")
+readarray PACKAGES_IMPORT < <(yq '.imports.packages[]' "$CONFIGURATIONS_FILE")
 
 for package in "${PACKAGES_IMPORT[@]}"; do
 
